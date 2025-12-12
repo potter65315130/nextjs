5b5355e46d0fb57480677fd5eb381a9248f23a81
sittinon <sittinonn65@gmail.com>
2025-12-12 10:45:16 +0700

    ลบ

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -108,7 +108,6 @@ export default function LoginPage() {
                     />
                 </AuthCard>
             </AuthBackground>
-            <Footer />
         </>
     );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -131,7 +131,6 @@ export default function RegisterPage() {
                     />
                 </AuthCard>
             </AuthBackground>
-            <Footer />
         </>
     );
 }
